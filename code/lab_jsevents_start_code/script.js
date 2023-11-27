7e20940b453a0ffae141c3ce8286edd4fbd3e6e4
Emily Reid <emily.reid31@gmail.com>
2023-11-27 15:36:20 +0000

    added basic css background, buttons for completing and deleting tasks

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -12,5 +12,25 @@ textInput.addEventListener('input', (event) => {
     const newListItem = document.createElement("li");
     newListItem.innerText = event.target.value;
     todolist.appendChild(newListItem);
+    // newListItem.appendChild(deleteButton);
 })
 
+const createCompleteButton = (toDoItem) => {
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "Completed";
+    completeButton.addEventListener('click', (event) => {
+        markAsCompleted(toDoItem);
+    })
+        return completeButton;
+
+    }
+
+const createDeleteButton = (toDoItem) => {
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', (event) => {
+        toDoItem.remove();
+        });
+
+    return deleteButton;
+}
